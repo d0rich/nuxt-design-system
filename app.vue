8414582265fa1c344e9bf7d3851a5b9de6d2e2b7
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-22 22:00:18 +0400

    Implament example demostration

--- app.vue
+++ app.vue
@@ -1,8 +1,30 @@
+<script setup lang="ts">
+import { CharacterPose } from './components/d/Character.vue'
+import { ActionListItem } from './components/d/actions/List.vue'
+
+const pose = ref<CharacterPose>('idle')
+const poseOptions = ref<ActionListItem<CharacterPose>[]>([
+  { title: 'Idle', emit: 'idle' },
+  { title: 'Action', emit: 'action' },
+  { title: 'Profi', emit: 'profi' }
+])
+</script>
+
 <template>
   <Head>
     <Title>d0rich • Nuxt design system</Title>
   </Head>
-  <div>
-    <h1 class="sharp-shadow ss-red-500 ss-b-2">d0rich Nuxt design system</h1>
-  </div>
+  <main class="w-full min-h-screen bg-neutral-300 px-3 py-10">
+    <div class="prose prose-lg mx-auto">
+      <h1 class="sharp-shadow ss-red-500 ss-br-1">d0rich Nuxt design system</h1>
+      <section class="grid md:grid-cols-2 not-prose items-center">
+        <DCharacter :pose="pose" />
+        <DActionsList
+          class="text-white"
+          :actions="poseOptions"
+          @action-focus="pose = $event"
+        />
+      </section>
+    </div>
+  </main>
 </template>
