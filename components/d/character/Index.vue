2c5e8fd9cc9dcff90c835f97db27a1282b4f8ffa
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-22 22:54:59 +0400

    Rewrite Character to composition API

--- components/d/character/Index.vue
+++ components/d/character/Index.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import idle from '../../../assets/img/character/idle.webp'
+import idleColor from '../../../assets/img/character/idle-color.webp'
+import idleOutline from '../../../assets/img/character/idle-outline.webp'
+import idleOutlineColor from '../../../assets/img/character/idle-outline-color.webp'
+import action from '../../../assets/img/character/action.webp'
+import actionColor from '../../../assets/img/character/action-color.webp'
+import actionOutline from '../../../assets/img/character/action-outline.webp'
+import actionOutlineColor from '../../../assets/img/character/action-outline-color.webp'
+import profi from '../../../assets/img/character/profi.webp'
+import profiColor from '../../../assets/img/character/profi-color.webp'
+import profiOutline from '../../../assets/img/character/profi-outline.webp'
+import profiOutlineColor from '../../../assets/img/character/profi-outline-color.webp'
+
+export type CharacterPose = 'idle' | 'action' | 'profi'
+
+const props = defineProps({
+  pose: {
+    type: String as () => CharacterPose,
+    required: true
+  },
+  outline: Boolean,
+  color: Boolean
+})
+
+const assets = {
+  idle,
+  idleColor,
+  idleOutline,
+  idleOutlineColor,
+  action,
+  actionColor,
+  actionOutline,
+  actionOutlineColor,
+  profi,
+  profiColor,
+  profiOutline,
+  profiOutlineColor
+}
+
+function getAsset(pose: CharacterPose) {
+  return assets[
+    `${pose}${props.outline ? 'Outline' : ''}${props.color ? 'Color' : ''}`
+  ]
+}
+</script>
+
 <template>
   <div>
     <div class="relative isolate">
@@ -28,59 +75,6 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import idle from '../../../assets/img/character/idle.webp'
-import idleColor from '../../../assets/img/character/idle-color.webp'
-import idleOutline from '../../../assets/img/character/idle-outline.webp'
-import idleOutlineColor from '../../../assets/img/character/idle-outline-color.webp'
-import action from '../../../assets/img/character/action.webp'
-import actionColor from '../../../assets/img/character/action-color.webp'
-import actionOutline from '../../../assets/img/character/action-outline.webp'
-import actionOutlineColor from '../../../assets/img/character/action-outline-color.webp'
-import profi from '../../../assets/img/character/profi.webp'
-import profiColor from '../../../assets/img/character/profi-color.webp'
-import profiOutline from '../../../assets/img/character/profi-outline.webp'
-import profiOutlineColor from '../../../assets/img/character/profi-outline-color.webp'
-
-export type CharacterPose = 'idle' | 'action' | 'profi'
-
-export default defineComponent({
-  props: {
-    pose: {
-      type: String as () => CharacterPose,
-      required: true
-    },
-    outline: Boolean,
-    color: Boolean
-  },
-  setup(props) {
-    const assets = {
-      idle,
-      idleColor,
-      idleOutline,
-      idleOutlineColor,
-      action,
-      actionColor,
-      actionOutline,
-      actionOutlineColor,
-      profi,
-      profiColor,
-      profiOutline,
-      profiOutlineColor
-    }
-    return {
-      assets,
-      getAsset: (pose: CharacterPose) =>
-        assets[
-          `${pose}${props.outline ? 'Outline' : ''}${
-            props.color ? 'Color' : ''
-          }`
-        ]
-    }
-  }
-})
-</script>
 
 <style scoped>
 img {
