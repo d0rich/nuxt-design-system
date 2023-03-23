28db8dce668d955f1eb62c951aaffb8f52bdcc2c
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-23 20:21:19 +0400

    Create index of character component

---
+++ components/d/character/index.ts
@@ -0,0 +1,11 @@
+import ShapeIdle from "./shape/Idle.vue";
+import ShapeAction from "./shape/Action.vue";
+import ShapeProfi from "./shape/Profi.vue";
+import Character from './Index.vue'
+
+export { type CharacterPose } from './Index.vue'
+
+export const DCharacter = Character
+export const DCharacterShapeIdle = ShapeIdle
+export const DCharacterShapeAction = ShapeAction
+export const DCharacterShapeProfi = ShapeProfi
