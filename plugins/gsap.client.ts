e7860462ccc00018709632ec51365446dfa16772
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-23 17:55:16 +0400

    Use paid GSAP

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -1,9 +1,9 @@
 import gsap from 'gsap'
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { MorphSVGPlugin } from 'gsap-trial/MorphSVGPlugin'
+import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin'
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   gsap.registerPlugin(MotionPathPlugin)
   gsap.registerPlugin(ScrollTrigger)
   gsap.registerPlugin(MorphSVGPlugin)
