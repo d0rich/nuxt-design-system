a47d0d573ea55765c20fb5aa3df1861c86d760ac
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-24 13:11:16 +0400

    Install fallback dependency

--- fallbackDependencies.mjs
+++ fallbackDependencies.mjs
@@ -1,11 +1,15 @@
-import { exec } from 'node:child_process'
+import { execSync } from 'node:child_process'
 import * as fs from 'fs'
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf-8' }))
 
 // Check if dependencies folders exist
 for (let dependency in packageJson.dependencies) {
-  const path = `./node_modules/${dependency}`
-  console.log(dependency, fs.existsSync(path))
+  const isInstalled = fs.existsSync(`./node_modules/${dependency}`)
+  console.log(dependency, isInstalled)
+  if (!isInstalled) {
+    console.log(`Installing ${dependency} from optionalDependencies`)
+    execSync(`npm install ${dependency}@${packageJson.optionalDependencies[dependency]}`)
+  }
 }
 
