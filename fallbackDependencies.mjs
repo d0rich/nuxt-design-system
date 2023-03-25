738abf4a459ca9d6c39cf1493d70336944673ffd
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-25 20:56:35 +0400

    Return simple fallback dependency

--- fallbackDependencies.mjs
+++ fallbackDependencies.mjs
@@ -6,35 +6,37 @@ const packageJson = JSON.parse(
   fs.readFileSync('./package.json', { encoding: 'utf-8' })
 )
 
+console.log(process.env.GSAP_TOKEN)
+
 // Check if dependencies folders exist
-for (let dependency in packageJson.optionalDependencies) {
-  function isInstalled() {
-    return fs.existsSync(`./node_modules/${dependency}`)
-  }
-  if (!isInstalled()) {
-    consola.info(`Installing ${dependency} from fallbackDependencies`)
-    const fallbackDependency = packageJson.fallbackDependencies[dependency]
-    if (Array.isArray(fallbackDependency)) {
-      for (let version of fallbackDependency) {
-        const packageToInstall = `${dependency}@${version}`
-        execSync(`npm install ${packageToInstall} --no-save`)
-        if (isInstalled()) {
-          consola.info(`${packageToInstall} is installed`)
-          break
-        }
-        consola.error(`Failed to install ${packageToInstall}`)
-      }
-    } else if (typeof fallbackDependency === 'string') {
-      const packageToInstall = `${dependency}@${fallbackDependency}`
-      execSync(`npm install ${packageToInstall} --no-save`)
-      if (isInstalled()) {
-        consola.info(`${packageToInstall} is installed`)
-      } else {
-        consola.error(`Failed to install ${packageToInstall}`)
-      }
-    }
-  }
-}
+// for (let dependency in packageJson.optionalDependencies) {
+//   function isInstalled() {
+//     return fs.existsSync(`./node_modules/${dependency}`)
+//   }
+//   if (!isInstalled()) {
+//     consola.info(`Installing ${dependency} from fallbackDependencies`)
+//     const fallbackDependency = packageJson.fallbackDependencies[dependency]
+//     if (Array.isArray(fallbackDependency)) {
+//       for (let version of fallbackDependency) {
+//         const packageToInstall = `${dependency}@${version}`
+//         execSync(`npm install ${packageToInstall} --no-save`)
+//         if (isInstalled()) {
+//           consola.info(`${packageToInstall} is installed`)
+//           break
+//         }
+//         consola.error(`Failed to install ${packageToInstall}`)
+//       }
+//     } else if (typeof fallbackDependency === 'string') {
+//       const packageToInstall = `${dependency}@${fallbackDependency}`
+//       execSync(`npm install ${packageToInstall} --no-save`)
+//       if (isInstalled()) {
+//         consola.info(`${packageToInstall} is installed`)
+//       } else {
+//         consola.error(`Failed to install ${packageToInstall}`)
+//       }
+//     }
+//   }
+// }
 
 // Manually create files to avoid Rollup error
 const files = {
