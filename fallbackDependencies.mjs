86a6a3655229c93712f2cdf1c9ef33ef1067e68a
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-25 14:53:24 +0400

    Nuxt layer fallback hotfix (#9)

--- fallbackDependencies.mjs
+++ fallbackDependencies.mjs
@@ -2,24 +2,14 @@ import { execSync } from 'node:child_process'
 import * as fs from 'fs'
 import consola from 'consola'
 
-const prefix = 'node_modules/@d0rich/nuxt-desing-system'
-
-const isNuxtLayer = fs.existsSync(`./${prefix}`)
-
 const packageJson = JSON.parse(
-  fs.readFileSync(isNuxtLayer ? `./${prefix}/package.json` : `./package.json`, {
-    encoding: 'utf-8'
-  })
+  fs.readFileSync('./package.json', { encoding: 'utf-8' })
 )
 
 // Check if dependencies folders exist
 for (let dependency in packageJson.optionalDependencies) {
   function isInstalled() {
-    return fs.existsSync(
-      isNuxtLayer
-        ? `./${prefix}/node_modules/${dependency}`
-        : `./node_modules/${dependency}`
-    )
+    return fs.existsSync(`./node_modules/${dependency}`)
   }
   if (!isInstalled()) {
     consola.info(`Installing ${dependency} from fallbackDependencies`)
@@ -27,24 +17,16 @@ for (let dependency in packageJson.optionalDependencies) {
     if (Array.isArray(fallbackDependency)) {
       for (let version of fallbackDependency) {
         const packageToInstall = `${dependency}@${version}`
-        execSync(
-          `npm install ${packageToInstall} --no-save ${
-            isNuxtLayer ? `--prefix ./${prefix}` : ''
-          }`
-        )
+        execSync(`npm install ${packageToInstall} --no-save`)
         if (isInstalled()) {
           consola.info(`${packageToInstall} is installed`)
           break
         }
-        consola.error(`Failed to install ${packageToInstall} `)
+        consola.error(`Failed to install ${packageToInstall}`)
       }
     } else if (typeof fallbackDependency === 'string') {
       const packageToInstall = `${dependency}@${fallbackDependency}`
-      execSync(
-        `npm install ${packageToInstall} --no-save ${
-          isNuxtLayer ? `--prefix ./${prefix}` : ''
-        }`
-      )
+      execSync(`npm install ${packageToInstall} --no-save`)
       if (isInstalled()) {
         consola.info(`${packageToInstall} is installed`)
       } else {
@@ -55,19 +37,14 @@ for (let dependency in packageJson.optionalDependencies) {
 }
 
 // Manually create files to avoid Rollup error
-const files = [
-  {
-    path: `./${
-      isNuxtLayer ? `${prefix}/` : ''
-    }node_modules/gsap/MorphSVGPlugin.js`,
-    content: 'export const MorphSVGPlugin = {}'
-  }
-]
-for (let { path, content } of files) {
+const files = {
+  './node_modules/gsap/MorphSVGPlugin.js': 'export const MorphSVGPlugin = {}'
+}
+for (let path in files) {
   if (!fs.existsSync(path)) {
     const dirPath = path.split('/').slice(0, -1).join('/')
     if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
-    fs.writeFileSync(path, content, {
+    fs.writeFileSync(path, files[path], {
       encoding: 'utf-8'
     })
   }
