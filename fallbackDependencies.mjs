3c242c1e8a48dc67a47f08c32962d2e8f9657fb3
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-25 12:07:19 +0400

    Try custom package.json field

--- fallbackDependencies.mjs
+++ fallbackDependencies.mjs
@@ -1,5 +1,6 @@
 import { execSync } from 'node:child_process'
 import * as fs from 'fs'
+import consola from 'consola'
 
 const packageJson = JSON.parse(
   fs.readFileSync('./package.json', { encoding: 'utf-8' })
@@ -8,21 +9,25 @@ const packageJson = JSON.parse(
 // Check if dependencies folders exist
 for (let dependency in packageJson.dependencies) {
   const isInstalled = fs.existsSync(`./node_modules/${dependency}`)
-  console.log(dependency, isInstalled)
   if (!isInstalled) {
-    console.log(`Installing ${dependency} from optionalDependencies`)
+    consola.info(`Installing ${dependency} from fallbackDependencies`)
     execSync(
-      `npm install ${dependency}@${packageJson.optionalDependencies[dependency]}`
+      `npm install ${dependency}@${packageJson.fallbackDependencies[dependency]}`
     )
   }
 }
 
 // Manually create files to avoid Rollup error
-const path = './node_modules/gsap/MorphSVGPlugin.js'
-if (!fs.existsSync(path)) {
-  const dirPath = path.split('/').slice(0, -1).join('/')
-  if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
-  fs.writeFileSync(path, 'export const MorphSVGPlugin = {}', {
-    encoding: 'utf-8'
-  })
+const files = {
+  './node_modules/gsap/MorphSVGPlugin.js': 'export const MorphSVGPlugin = {}'
 }
+for (let path in files) {
+  if (!fs.existsSync(path)) {
+    const dirPath = path.split('/').slice(0, -1).join('/')
+    if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
+    fs.writeFileSync(path, files[path], {
+      encoding: 'utf-8'
+    })
+  }
+}
+
