33c440e8dff492de5351f68fcef2dbdb096498d2
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-25 13:05:00 +0400

    Update fallback dependencies script (#6)
    * Try custom package.json field
    
    * Private GSAP is optional dependency
    
    * Multiple fallback dependencies
    
    * Fix postinstall script
    
    * Fix postinstall script
    
    * Check if packeage is installed
    
    * Update check of fallback dependencies
    
    * Update version
    
    * Shockingly is target GSAP version

--- fallbackDependencies.mjs
+++ fallbackDependencies.mjs
@@ -1,28 +1,51 @@
 import { execSync } from 'node:child_process'
 import * as fs from 'fs'
+import consola from 'consola'
 
 const packageJson = JSON.parse(
   fs.readFileSync('./package.json', { encoding: 'utf-8' })
 )
 
 // Check if dependencies folders exist
-for (let dependency in packageJson.dependencies) {
-  const isInstalled = fs.existsSync(`./node_modules/${dependency}`)
-  console.log(dependency, isInstalled)
-  if (!isInstalled) {
-    console.log(`Installing ${dependency} from optionalDependencies`)
-    execSync(
-      `npm install ${dependency}@${packageJson.optionalDependencies[dependency]}`
-    )
+for (let dependency in packageJson.optionalDependencies) {
+  function isInstalled() {
+    return fs.existsSync(`./node_modules/${dependency}`)
+  }
+  if (!isInstalled()) {
+    consola.info(`Installing ${dependency} from fallbackDependencies`)
+    const fallbackDependency = packageJson.fallbackDependencies[dependency]
+    if (Array.isArray(fallbackDependency)) {
+      for (let version of fallbackDependency) {
+        const packageToInstall = `${dependency}@${version}`
+        execSync(`npm install ${packageToInstall} --no-save`)
+        if (isInstalled()) {
+          consola.info(`${packageToInstall} is installed`)
+          break
+        }
+        consola.error(`Failed to install ${packageToInstall}`)
+      }
+    } else if (typeof fallbackDependency === 'string') {
+      const packageToInstall = `${dependency}@${fallbackDependency}`
+      execSync(`npm install ${packageToInstall} --no-save`)
+      if (isInstalled()) {
+        consola.info(`${packageToInstall} is installed`)
+      } else {
+        consola.error(`Failed to install ${packageToInstall}`)
+      }
+    }
   }
 }
 
 // Manually create files to avoid Rollup error
-const path = './node_modules/gsap/MorphSVGPlugin.js'
-if (!fs.existsSync(path)) {
-  const dirPath = path.split('/').slice(0, -1).join('/')
-  if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
-  fs.writeFileSync(path, 'export const MorphSVGPlugin = {}', {
-    encoding: 'utf-8'
-  })
+const files = {
+  './node_modules/gsap/MorphSVGPlugin.js': 'export const MorphSVGPlugin = {}'
+}
+for (let path in files) {
+  if (!fs.existsSync(path)) {
+    const dirPath = path.split('/').slice(0, -1).join('/')
+    if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
+    fs.writeFileSync(path, files[path], {
+      encoding: 'utf-8'
+    })
+  }
 }
