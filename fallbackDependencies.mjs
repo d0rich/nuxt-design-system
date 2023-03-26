6cd6dc77460b9e3af5b6bfe54cf052ac12d44198
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-26 16:12:06 +0400

    Specify GSAP as peerDependency (#15)
    * Specify GSAP as peerDependency
    
    * Use npm ci in automation
    
    * postinstall create fallback for paid gsap plugins
    
    * Return `gsap` to dependencies for development
    
    * Update version
    
    * Install optional dependency

--- fallbackDependencies.mjs
+++ fallbackDependencies.mjs
@@ -1,3 +1,21 @@
+/**
+ * This script was used as `postinstall` script to install fallback GSAP if payed one is not available.
+ * But it seems project scoped `.npmrc` can't be acessed if this repository is used as dependency.
+ *
+ * Code for package.json
+ * ```
+ * "optionalDependencies": {
+ *  "gsap": "npm:@gsap/shockingly@^3.11.5"
+ *  },
+ *  "fallbackDependencies": {
+ *    "gsap": [
+ *      "npm:@gsap/simply@^3.11.5",
+ *      "^3.11.5"
+ *    ]
+ *  }
+ * ```
+ */
+
 import { execSync } from 'node:child_process'
 import * as fs from 'fs'
 import { resolve, sep, join } from 'node:path'
@@ -18,8 +36,43 @@ const root = currentDir
   .slice(0, firstNodeModules === -1 ? undefined : firstNodeModules)
   .join(sep)
 
-// Check if dependencies folders exist
-for (let dependency in packageJson.optionalDependencies) {
+// // Work with custom fallbackDependencies
+// for (let dependency in packageJson.optionalDependencies) {
+//   function isInstalled() {
+//     return fs.existsSync(join(root, `./node_modules/${dependency}`))
+//   }
+
+//   function install(packageToInstall) {
+//     execSync(`npm install ${packageToInstall} --no-save`, {
+//       cwd: root
+//     })
+
+//     const isInstalledResult = isInstalled()
+//     if (isInstalledResult) {
+//       consola.info(`${packageToInstall} is installed`)
+//     } else {
+//       consola.error(`Failed to install ${packageToInstall}`)
+//     }
+//     return isInstalledResult
+//   }
+
+//   if (!isInstalled()) {
+//     consola.info(`Installing ${dependency} from fallbackDependencies`)
+//     const fallbackDependency = packageJson.fallbackDependencies[dependency]
+
+//     if (Array.isArray(fallbackDependency)) {
+//       for (let version of fallbackDependency) {
+//         const installResult = install(`${dependency}@${version}`)
+//         if (installResult) break
+//       }
+//     } else if (typeof fallbackDependency === 'string') {
+//       install(`${dependency}@${fallbackDependency}`)
+//     }
+//   }
+// }
+
+// Work with optionalDependencies
+for (let dependency in packageJson.dependencies) {
   function isInstalled() {
     return fs.existsSync(join(root, `./node_modules/${dependency}`))
   }
@@ -40,16 +93,8 @@ for (let dependency in packageJson.optionalDependencies) {
 
   if (!isInstalled()) {
     consola.info(`Installing ${dependency} from fallbackDependencies`)
-    const fallbackDependency = packageJson.fallbackDependencies[dependency]
-
-    if (Array.isArray(fallbackDependency)) {
-      for (let version of fallbackDependency) {
-        const installResult = install(`${dependency}@${version}`)
-        if (installResult) break
-      }
-    } else if (typeof fallbackDependency === 'string') {
-      install(`${dependency}@${fallbackDependency}`)
-    }
+    const fallbackDependency = packageJson.optionalDependencies[dependency]
+    install(`${dependency}@${fallbackDependency}`)
   }
 }
 
