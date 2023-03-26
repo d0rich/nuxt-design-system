ad2c2f45b2b8ee57cd62e8fdf26d012d1f9da002
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-26 14:33:41 +0400

    Implement GSAP test for checking plugin

---
+++ tests/GSAP.spec.ts
@@ -0,0 +1,18 @@
+import { describe, test, expect } from 'vitest'
+import { setup, url, getBrowser } from '@nuxt/test-utils'
+
+describe('Test GSAP on page', async () => {
+  await setup({
+    // test context options
+  })
+  test('MorphSVGPlugin should be installed', async () => {
+    const browser = await getBrowser()
+    const page = await browser.newPage()
+    await page.goto(url('/'), {
+      waitUntil: 'networkidle'
+    })
+    const morphSVGTypeHandle = await page.evaluateHandle(() => Promise.resolve(typeof window?.gsap?.plugins?.morphSVG))
+    const morphSVGType = await morphSVGTypeHandle.jsonValue()
+    expect(morphSVGType).toBe('function')
+  })
+})
