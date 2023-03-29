a61dede7cae28ba4671ef0622ca4c4487f49a682
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-29 19:29:29 +0400

    Register GSAP SplitText plugin

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -3,11 +3,13 @@ import consola from 'consola/src/browser'
 import gsap from 'gsap'
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { SplitText } from 'gsap/SplitText'
 import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin'
 
 export default defineNuxtPlugin(() => {
   gsap.registerPlugin(MotionPathPlugin)
   gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(SplitText)
   gsap.registerPlugin(MorphSVGPlugin)
   if (!isMorphSVGPluginInstalled())
     consola.warn('Free GSAP is used. Morph SVG plugin is ignored.')
