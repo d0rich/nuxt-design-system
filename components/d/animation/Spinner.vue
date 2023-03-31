fbccb74e56f2fddbfa0c75b364dd2e9ac93ce7de
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-31 18:13:00 +0400

    spinner draft

--- components/d/animation/Spinner.vue
+++ components/d/animation/Spinner.vue
@@ -4,11 +4,39 @@ export default {
 }
 </script>
 
+<script setup lang="ts">
+const transformOrigin = '315 315'
+</script>
+
 <template>
   <svg class="spinner" viewBox="0 0 630 630" xmlns="http://www.w3.org/2000/svg">
     <defs>
-      <DAnimationDefLetterD id="base" />
+      <DAnimationDefLetterD id="d-blade" transform="scale(0.5)" :transform-origin="transformOrigin" />
+      <g id="d-blade--shadow">
+        <use href="#base" transform="translate( -10 10 )" class="fill-black" />
+        <use href="#base" class="fill-white" />
+      </g>
+      <g id="spinner" class="d-animation-spinner__spinner" :transform-origin="transformOrigin">
+        <use href="#d-blade" transform="translate(0 -150) rotate(240)"  :transform-origin="transformOrigin" />
+        <use href="#d-blade" transform="translate(-129.9 75) rotate(120)"  :transform-origin="transformOrigin" />
+        <use href="#d-blade" transform="translate(129.9 75) rotate(300)"  :transform-origin="transformOrigin" />
+      </g>
     </defs>
-    <use href="#base" class="fill-white" />
+    <use href="#spinner" transform="translate( -20 20 )" class="fill-black" :transform-origin="transformOrigin" />
+    <use href="#spinner" class="fill-white" :transform-origin="transformOrigin" />
   </svg>
 </template>
+
+<style>
+.d-animation-spinner__spinner {
+  animation: spin 2s linear infinite;
+}
+@keyframes spin  {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+</style>
