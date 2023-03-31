9dc6f3553b565c22c9d67ff5ffcee0cf1714d108
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-31 20:35:26 +0400

    Configurable spinner

--- components/d/animation/Spinner.vue
+++ components/d/animation/Spinner.vue
@@ -6,13 +6,26 @@ export default {
 
 <script setup lang="ts">
 const transformOrigin = '315 315'
+defineProps({
+  spinPeriod: {
+    type: Number,
+    default: 10
+  },
+  bladeWigglePeriod: {
+    type: Number,
+    default: 5
+  }
+})
 </script>
 
 <template>
-  <svg class="spinner" viewBox="0 0 630 630" xmlns="http://www.w3.org/2000/svg">
+  <svg class="spinner" viewBox="0 0 630 630" xmlns="http://www.w3.org/2000/svg" :style="{
+    '--d-spinner-spin-period': spinPeriod + 's',
+    '--d-spinner-blade-wiggle-period': bladeWigglePeriod + 's'
+  }">
     <defs>
       <DAnimationDefLetterD id="d-letter" />
-      <use href="#d-letter" id="d-blade" transform="translate(0 -150) scale(0.5) rotate(220)" :transform-origin="transformOrigin" />
+      <use href="#d-letter" id="d-blade" class="d-animation-spinner__blade" :transform-origin="transformOrigin" />
       <g id="spinner" class="d-animation-spinner__spinner" :transform-origin="transformOrigin">
         <use href="#d-blade" transform="rotate(0)" :transform-origin="transformOrigin" />
         <use href="#d-blade" transform="rotate(120)" :transform-origin="transformOrigin" />
@@ -25,15 +38,30 @@ const transformOrigin = '315 315'
 </template>
 
 <style>
-.d-animation-spinner__spinner {
-  animation: spin 2s linear infinite;
+.d-animation-spinner__blade {
+  --transform-static: scale(0.5) rotate(220deg);
+  animation: d-spinner-blade-wiggle var(--d-spinner-blade-wiggle-period) ease-in-out infinite;
 }
-@keyframes spin  {
+@keyframes d-spinner-blade-wiggle {
+  0% {
+    transform: translate(0px, -150px) var(--transform-static);
+  }
+  50% {
+    transform: translate(0px, -120px) var(--transform-static);
+  }
+  100% {
+    transform: translate(0px, -150px) var(--transform-static);
+  }
+}
+.d-animation-spinner__spinner {
+  animation: d-spinner-spin var(--d-spinner-spin-period) linear infinite;
+}
+@keyframes d-spinner-spin  {
   0% {
     transform: rotate(0deg);
   }
   100% {
-    transform: rotate(360deg);
+    transform: rotate(-360deg);
   }
 }
 </style>
