49e15ff65fa039bbac4591ff1677a32d5b732178
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-31 20:36:04 +0400

    lintfix

--- components/d/animation/Spinner.vue
+++ components/d/animation/Spinner.vue
@@ -19,28 +19,64 @@ defineProps({
 </script>
 
 <template>
-  <svg class="spinner" viewBox="0 0 630 630" xmlns="http://www.w3.org/2000/svg" :style="{
-    '--d-spinner-spin-period': spinPeriod + 's',
-    '--d-spinner-blade-wiggle-period': bladeWigglePeriod + 's'
-  }">
+  <svg
+    class="spinner"
+    viewBox="0 0 630 630"
+    xmlns="http://www.w3.org/2000/svg"
+    :style="{
+      '--d-spinner-spin-period': spinPeriod + 's',
+      '--d-spinner-blade-wiggle-period': bladeWigglePeriod + 's'
+    }"
+  >
     <defs>
       <DAnimationDefLetterD id="d-letter" />
-      <use href="#d-letter" id="d-blade" class="d-animation-spinner__blade" :transform-origin="transformOrigin" />
-      <g id="spinner" class="d-animation-spinner__spinner" :transform-origin="transformOrigin">
-        <use href="#d-blade" transform="rotate(0)" :transform-origin="transformOrigin" />
-        <use href="#d-blade" transform="rotate(120)" :transform-origin="transformOrigin" />
-        <use href="#d-blade" transform="rotate(240)" :transform-origin="transformOrigin" />
+      <use
+        id="d-blade"
+        href="#d-letter"
+        class="d-animation-spinner__blade"
+        :transform-origin="transformOrigin"
+      />
+      <g
+        id="spinner"
+        class="d-animation-spinner__spinner"
+        :transform-origin="transformOrigin"
+      >
+        <use
+          href="#d-blade"
+          transform="rotate(0)"
+          :transform-origin="transformOrigin"
+        />
+        <use
+          href="#d-blade"
+          transform="rotate(120)"
+          :transform-origin="transformOrigin"
+        />
+        <use
+          href="#d-blade"
+          transform="rotate(240)"
+          :transform-origin="transformOrigin"
+        />
       </g>
     </defs>
-    <use href="#spinner" transform="translate( -20 20 )" class="fill-black" :transform-origin="transformOrigin" />
-    <use href="#spinner" class="fill-white" :transform-origin="transformOrigin" />
+    <use
+      href="#spinner"
+      transform="translate( -20 20 )"
+      class="fill-black"
+      :transform-origin="transformOrigin"
+    />
+    <use
+      href="#spinner"
+      class="fill-white"
+      :transform-origin="transformOrigin"
+    />
   </svg>
 </template>
 
 <style>
 .d-animation-spinner__blade {
   --transform-static: scale(0.5) rotate(220deg);
-  animation: d-spinner-blade-wiggle var(--d-spinner-blade-wiggle-period) ease-in-out infinite;
+  animation: d-spinner-blade-wiggle var(--d-spinner-blade-wiggle-period)
+    ease-in-out infinite;
 }
 @keyframes d-spinner-blade-wiggle {
   0% {
@@ -56,7 +92,7 @@ defineProps({
 .d-animation-spinner__spinner {
   animation: d-spinner-spin var(--d-spinner-spin-period) linear infinite;
 }
-@keyframes d-spinner-spin  {
+@keyframes d-spinner-spin {
   0% {
     transform: rotate(0deg);
   }
