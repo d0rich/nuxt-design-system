9b13e8a664c41eb903b89a09da46b7a1a98edb26
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-31 14:46:54 +0400

    Rewrite BigBangButton Animation with setup script

--- components/d/big-bang-button/Animation.vue
+++ components/d/big-bang-button/Animation.vue
@@ -1,3 +1,66 @@
+<script setup lang="ts">
+import gsap from 'gsap'
+
+const paths = {
+  first: 'M 0 0 C -489.471 -116.5 -526.692 -193.268 -563.913 -311.911',
+  last: 'M 0 0 C 317.761 171.963 466.645 199.879 680.666 171.963',
+  intermediate: [
+    'M 0 0 C 36.277 125.437 19.992 358.068 -49.797 413.9',
+    'M 0 0 C 52.561 -69.974 71.171 -118.827 64.192 -446.837',
+    'M 0 0 C -3.271 137.068 -26.534 395.289 33.95 439.489'
+  ]
+}
+
+const { text } = useDBigBangButtonUtils()
+const symbols = computed(() => text.value.split(''))
+
+let clearTask: NodeJS.Timeout
+
+watch(text, (val) => {
+  clearTimeout(clearTask)
+  if (val) clearTask = setTimeout(() => (text.value = ''), 1500)
+})
+function pathFromIndex(i: number) {
+  if (i === 0) return paths.first
+  if (i === symbols.value.length - 1) return paths.last
+  return paths.intermediate[
+    Math.floor(Math.random() * paths.intermediate.length)
+  ]
+  // return paths.intermediate[i % paths.intermediate.length]
+}
+
+function onEnter(el: Element, done: () => void) {
+  gsap.fromTo(
+    el,
+    {
+      opacity: 0,
+      rotateX: -90
+    },
+    {
+      opacity: 1,
+      rotateX: 0,
+      rotate: -12 + Math.random() * 24,
+      delay: Math.random() * 0.3,
+      duration: 0.2,
+      onComplete: done
+    }
+  )
+}
+
+function onLeave(el: Element, done: () => void) {
+  const index = +gsap.getProperty(el, 'i')
+  gsap.to(el, {
+    motionPath: {
+      path: pathFromIndex(index)
+    },
+    opacity: 0,
+    rotate: -180 + Math.random() * 360,
+    duration: 0.2,
+    onComplete: done
+  })
+}
+</script>
+
 <template>
   <div id="big-bang-button-animation">
     <TransitionGroup
@@ -15,72 +78,6 @@
     </TransitionGroup>
   </div>
 </template>
-
-<script lang="ts">
-import gsap from 'gsap'
-
-const paths = {
-  first: 'M 0 0 C -489.471 -116.5 -526.692 -193.268 -563.913 -311.911',
-  last: 'M 0 0 C 317.761 171.963 466.645 199.879 680.666 171.963',
-  intermediate: [
-    'M 0 0 C 36.277 125.437 19.992 358.068 -49.797 413.9',
-    'M 0 0 C 52.561 -69.974 71.171 -118.827 64.192 -446.837',
-    'M 0 0 C -3.271 137.068 -26.534 395.289 33.95 439.489'
-  ]
-}
-
-export default defineComponent({
-  setup() {
-    const { text } = useDBigBangButtonUtils()
-    const symbols = computed(() => text.value.split(''))
-    let clearTask: NodeJS.Timeout
-    watch(text, (val) => {
-      clearTimeout(clearTask)
-      if (val) clearTask = setTimeout(() => (text.value = ''), 1500)
-    })
-    function pathFromIndex(i: number) {
-      if (i === 0) return paths.first
-      if (i === symbols.value.length - 1) return paths.last
-      return paths.intermediate[
-        Math.floor(Math.random() * paths.intermediate.length)
-      ]
-      // return paths.intermediate[i % paths.intermediate.length]
-    }
-    return {
-      symbols,
-      onEnter(el: Element, done: () => void) {
-        gsap.fromTo(
-          el,
-          {
-            opacity: 0,
-            rotateX: -90
-          },
-          {
-            opacity: 1,
-            rotateX: 0,
-            rotate: -12 + Math.random() * 24,
-            delay: Math.random() * 0.3,
-            duration: 0.2,
-            onComplete: done
-          }
-        )
-      },
-      onLeave(el: Element, done: () => void) {
-        const index = +gsap.getProperty(el, 'i')
-        gsap.to(el, {
-          motionPath: {
-            path: pathFromIndex(index)
-          },
-          opacity: 0,
-          rotate: -180 + Math.random() * 360,
-          duration: 0.2,
-          onComplete: done
-        })
-      }
-    }
-  }
-})
-</script>
 
 <style>
 #big-bang-button-animation {
